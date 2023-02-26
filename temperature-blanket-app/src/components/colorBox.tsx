ac6828d5ff65c06bb3f5ef0efab7fb90ecb34abc
NicoleSelig <nlselig@sep.com>
2023-02-26 10:52:39 -0500

    color changes on click

--- temperature-blanket-app/src/components/colorBox.tsx
+++ temperature-blanket-app/src/components/colorBox.tsx
@@ -1,45 +1,46 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 
-export default class ColorBox extends Component {
-    color: string
+function generateRandomRGB() {
+    return Math.floor(Math.random() * 256)
+}
 
-    constructor(props: any) {
-        super(props)
-        this.color = this.generateColor()
+function generateColor() {
+    const r = generateRandomRGB()
+    const g = generateRandomRGB()
+    const b = generateRandomRGB()
+    return `rgb(${r},${g},${b})`
+}
+
+function RGBToHex(rgb: any) {
+    const seperator = rgb.indexOf(",") > -1 ? "," : ""
+    const newRGB = rgb.substr(4).split(")")[0].split(seperator)
+    let r = (+newRGB[0]).toString(16)
+    let g = (+newRGB[1]).toString(16)
+    let b = (+newRGB[2]).toString(16)
+
+    if(r.length === 1) r = "0" + r
+    if(g.length === 1) r = "0" + g
+    if(b.length === 1) r = "0" + b
+
+    return `#${r}${g}${b}`.toUpperCase()
+}
+
+// handleBoxClick() {
+//     this.setState({color: this.generateColor()})
+// }
+
+export default function ColorBox() {
+    const [color, setColor] = useState(generateColor())
+
+    const handleBoxClick = () => {
+        setColor(generateColor())
     }
 
-    generateRandomRGB() {
-        return Math.floor(Math.random() * 256)
-    }
-
-    generateColor() {
-        const r = this.generateRandomRGB()
-        const g = this.generateRandomRGB()
-        const b = this.generateRandomRGB()
-        return `rgb(${r},${g},${b})`
-    }
-
-    RGBToHex(rgb: any) {
-        const seperator = rgb.indexOf(",") > -1 ? "," : ""
-        const newRGB = rgb.substr(4).split(")")[0].split(seperator)
-        let r = (+newRGB[0]).toString(16)
-        let g = (+newRGB[1]).toString(16)
-        let b = (+newRGB[2]).toString(16)
-
-        if(r.length === 1) r = "0" + r
-        if(g.length === 1) r = "0" + g
-        if(b.length === 1) r = "0" + b
-
-        return `#${r}${g}${b}`.toUpperCase()
-    }
-
-    render(){
-        return(
-            <div className='bg-[green]' style={{backgroundColor: this.color}}>
-                <div className=''>
-                    {this.RGBToHex(this.color)}
-                </div>
+    return(
+        <div className='bg-[green]' style={{backgroundColor: color}} onClick={handleBoxClick}>
+            <div className=''>
+                {RGBToHex(color)}
             </div>
-        )
-    }
+        </div>
+    )
 }

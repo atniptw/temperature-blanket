154975cfb8a50e5b52c261b777a7defd79889c9d
NicoleSelig <nlselig@sep.com>
2023-02-26 14:31:30 -0500

    saving to local storage

--- temperature-blanket-app/src/components/form.tsx
+++ temperature-blanket-app/src/components/form.tsx
@@ -1,20 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 export default function Form() {
-    return (
-        <form className="m-auto grid sm:cols-span-1 md:cols-span-3 justify-center gap-4 mt-8">
-            <label>
-                <h2>Project Name</h2>
-                <input className="border-4 border-neutral-200" type="text"/>
-            </label>
-            <label>
-                <h2>Year</h2>
-                <input className="border-4 border-neutral-200" type="text"/>
-            </label>
-            <label>
-                <h2>Location</h2>
-                <input className="border-4 border-neutral-200" type="text"/>
-            </label>
-        </form>
-    )
+  const [ name, setName ] = useState('')
+  const [ year, setYear ] = useState('')
+  const [ locale, setLocale ] = useState('')
+
+  const handleProjectName = (e: any) => {
+    setName(e.target.value)
+  }
+
+  const handleYear = (e: any) => {
+    setYear(e.target.value)
+  }
+
+  const handleLocale = (e: any) => {
+    setLocale(e.target.value)
+  }
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    localStorage.setItem('projectName', name)
+    localStorage.setItem('year', year)
+    localStorage.setItem('location', locale)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="m-auto grid sm:cols-span-1 md:cols-span-3 justify-center gap-4 mt-8"
+    >
+      <label>
+        <h2>Project Name</h2>
+        <input
+          value={name || ''}
+          onChange={handleProjectName}
+          className="border-4 border-neutral-200"
+          type="text"
+        />
+      </label>
+      <label>
+        <h2>Year</h2>
+        <input
+          value={year || ''}
+          onChange={handleYear}
+          className="border-4 border-neutral-200"
+          type="text"
+        />
+      </label>
+      <label>
+        <h2>Location</h2>
+        <input
+          value={locale|| ''}
+          onChange={handleLocale}
+          className="border-4 border-neutral-200"
+          type="text"
+        />
+      </label>
+      <div className="flex justify-center">
+        <input
+          type="submit"
+          value="Start Knitting!"
+          className="mt-4 mb-4 border p-4 bg-[#323176] rounded-full hover:bg-pink-800 text-white font-bold"
+        />
+      </div>
+    </form>
+  );
 }
--- temperature-blanket-app/src/pages/index.tsx
+++ temperature-blanket-app/src/pages/index.tsx
@@ -11,11 +11,8 @@ export default function IndexPage() {
       <h1 className='text-3xl font-bold underline text-center'>Welcome to Tempestry!</h1>
       <p className='text-center'>A Temperature Blanket App</p>
     </main>
-    <Form/>
     <Pallette/>
-    <div className="flex justify-center">
-      <button className="mt-4 mb-4 border p-4 bg-[#323176] rounded-full hover:bg-pink-800 text-white font-bold">Start Knitting!</button>
-    </div>
+    <Form/>
     </>
   )
 }
